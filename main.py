b21a42b454617487bb968a40d1cab40b2ad5f26c
Tahiryar <tahiryar.251k@gmail.com>
2025-06-23 22:12:37 +0500

    make data set

--- main.py
+++ main.py
@@ -5,3 +5,12 @@ import google.generativeai as genai
 genai.configure(api_key="API_Key") 
 model = genai.GenerativeModel("gemini-1.5-flash")
 
+
+data = {
+    'Name': ['Tahir', 'Ali', 'Zara'],
+    'StudyHours': [6, 2, 4],
+    'Marks': [90, 55, 70]
+}
+df = pd.DataFrame(data)
+
+
